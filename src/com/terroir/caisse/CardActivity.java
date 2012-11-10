d0113b150314d1ed7abc87ed71c1fb93850c87e4
Yannick DOMINGUEZ <yannick.dominguez@gmail.com>
2012-11-10 16:30:17 +0100

    added text field for all items

--- src/com/terroir/caisse/CardActivity.java
+++ src/com/terroir/caisse/CardActivity.java
@@ -1,9 +1,18 @@
 package com.terroir.caisse;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.telephony.PhoneStateListener;
 import android.telephony.TelephonyManager;
@@ -18,6 +27,8 @@ public class CardActivity extends Activity {
 	
 	public static String TAG = CardActivity.class.getSimpleName();
 
+	private static String INSTAGRAM = "https://api.instagram.com/";
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -29,6 +40,19 @@ public class CardActivity extends Activity {
 			String name = intent.getStringExtra("name");
 			TextView text = (TextView) findViewById(R.id.txtViewName);
 			text.setText(name);
+			
+			String mail = intent.getStringExtra("mail");
+			TextView mailText = (TextView) findViewById(R.id.txtViewMail);
+			mailText.setText(mail);
+			
+			String category = intent.getStringExtra("category");
+			TextView categoryText = (TextView) findViewById(R.id.txtViewCategory);
+			categoryText.setText(category);
+
+			String website = intent.getStringExtra("website");
+			TextView websiteText = (TextView) findViewById(R.id.txtViewWebSite);
+			websiteText.setText(website);
+			
 		}catch(Exception e) {
 			e.printStackTrace();
 		}
@@ -45,7 +69,14 @@ public class CardActivity extends Activity {
 			    Intent intent = new Intent(Intent.ACTION_CALL, Uri.parse(url));			
 			    CardActivity.this.startActivity(intent);
 			}
-		});		
+		});	
+		
+		InstagramLoader loader = (InstagramLoader) new InstagramLoader().execute(INSTAGRAM);
+		
+	}
+	
+	private void onInstagramLoaded(String result) {
+		Log.i("", result);
 	}
 	
 	private class EndCallListener extends PhoneStateListener {
@@ -64,4 +95,70 @@ public class CardActivity extends Activity {
 	        }
 	    }
 	}
+	
+	
+	private class InstagramLoader extends AsyncTask<String, Void, String> {
+		
+		@Override
+	    protected String doInBackground(String... url) {
+	          
+	        // params comes from the execute() call: params[0] is the url.
+	        try {
+	            return downloadUrl(url[0]);
+	        } catch (IOException e) {
+	            return "Unable to retrieve web page. URL may be invalid.";
+	        }
+	    }
+	    // onPostExecute displays the results of the AsyncTask.
+	    @Override
+	    protected void onPostExecute(String result) {
+	    	onInstagramLoaded(result);
+	   }
+		
+		// Given a URL, establishes an HttpUrlConnection and retrieves
+		// the web page content as a InputStream, which it returns as
+		// a string.
+		private String downloadUrl(String urlAsString) throws IOException {
+		    InputStream is = null;
+		    // Only display the first 500 characters of the retrieved
+		    // web page content.
+		    int len = 500;
+		        
+		    try {
+		    	URL url = new URL(urlAsString);
+		        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+		        conn.setReadTimeout(10000 /* milliseconds */);
+		        conn.setConnectTimeout(15000 /* milliseconds */);
+		        conn.setRequestMethod("GET");
+		        conn.setDoInput(true);
+		        // Starts the query
+		        conn.connect();
+		        int response = conn.getResponseCode();
+		       
+		        is = conn.getInputStream();
+
+		        // Convert the InputStream into a string
+		        String contentAsString = readIt(is, len);
+		        return contentAsString;
+		        
+		    // Makes sure that the InputStream is closed after the app is
+		    // finished using it.
+		    } finally {
+		        if (is != null) {
+		            is.close();
+		        } 
+		   }
+		}
+		
+		// Reads an InputStream and converts it to a String.
+		public String readIt(InputStream stream, int len) throws IOException, UnsupportedEncodingException {
+		    Reader reader = null;
+		    reader = new InputStreamReader(stream, "UTF-8");        
+		    char[] buffer = new char[len];
+		    reader.read(buffer);
+		    return new String(buffer);
+		}
+
+	}
+
 }
